f0f67b6be8a92c33ac9e7f4181cb4e81e70b65e8
Matheus De Nigris <matheusdenigris.dev@gmail.com>
2025-12-02 03:23:03 +0000

    feat: add file `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,11 @@
-function status(request, response) {
-  response.status(200).json({ status: "ok, funcionando!" });
+import database from "../../../../infra/database";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  console.log(result.rows);
+  return response
+    .status(200)
+    .json({ status: "ok, funcionando!", db: result.rows });
 }
 
 export default status;
